3191db26454f62d73672361421ba138ed16f94a7
elyas-bhy <elyas.bhy@gmail.com>
2013-02-09 00:14:36 +0100

    Modifying subscriptions now properly updates filters.

--- source/src/com/dev/campus/util/Persistence.java
+++ source/src/com/dev/campus/util/Persistence.java
@@ -8,6 +8,8 @@ public class Persistence {
 	
 	public static final String PREF_SUBSCRIBE_UB1 = "subscribe_ub1";
 	public static final String PREF_SUBSCRIBE_LABRI = "subscribe_labri";
+	public static final String PREF_FILTER_UB1 = "filter_ub1";
+	public static final String PREF_FILTER_LABRI = "filter_labri";
 	
 	private SharedPreferences shared_prefs;
 	private SharedPreferences.Editor prefs_editor;
@@ -19,6 +21,10 @@ public class Persistence {
 	
 	public boolean isUBSubscribed() {
 		return shared_prefs.getBoolean(PREF_SUBSCRIBE_UB1, false);
+	}
+	
+	public boolean isLabriSubscribed() {
+		return shared_prefs.getBoolean(PREF_SUBSCRIBE_LABRI, false);
 	}
 	
 
--- source/src/com/dev/campus/util/PrefFragment.java
+++ source/src/com/dev/campus/util/PrefFragment.java
@@ -1,33 +1,70 @@
 package com.dev.campus.util;
 
-import com.dev.campus.CampusUB1App;
 import com.dev.campus.R;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.PreferenceFragment;
 
-public class PrefFragment extends PreferenceFragment {
+public class PrefFragment extends PreferenceFragment 
+implements SharedPreferences.OnSharedPreferenceChangeListener {
 
 	private CheckBoxPreference mPrefSubUB1;
 	private CheckBoxPreference mPrefSubLabri;
+	private CheckBoxPreference mPrefFilterUB1;
+	private CheckBoxPreference mPrefFilterLabri;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
-		CampusUB1App.LogD("PrefFragment created");
 		init();
 	}
 	
 	private void init() {
 		mPrefSubUB1 = (CheckBoxPreference) findPreference(Persistence.PREF_SUBSCRIBE_UB1);
 		mPrefSubLabri = (CheckBoxPreference) findPreference(Persistence.PREF_SUBSCRIBE_LABRI);
+		mPrefFilterUB1 = (CheckBoxPreference) findPreference(Persistence.PREF_FILTER_UB1);
+		mPrefFilterLabri = (CheckBoxPreference) findPreference(Persistence.PREF_FILTER_LABRI);
+		
+		if (!mPrefSubUB1.isChecked()) {
+			mPrefFilterUB1.setChecked(false);
+			mPrefFilterUB1.setEnabled(false);
+		}
+		
+		if (!mPrefSubLabri.isChecked()) {
+			mPrefFilterLabri.setChecked(false);
+			mPrefFilterLabri.setEnabled(false);
+		}
+		
+		getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(PrefFragment.this);
 	}
-	
+
 	@Override
-	public void onDestroy() {
-		super.onDestroy();
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {
+		if (key.equals(Persistence.PREF_SUBSCRIBE_UB1)) {
+			if (mPrefSubUB1.isChecked()) {
+				mPrefFilterUB1.setEnabled(true);
+				mPrefFilterUB1.setChecked(true);
+			}
+			else {
+				mPrefFilterUB1.setChecked(false);
+				mPrefFilterUB1.setEnabled(false);
+			}
+		}
+		
+		if (key.equals(Persistence.PREF_SUBSCRIBE_LABRI)) {
+			if (mPrefSubLabri.isChecked()) {
+				mPrefFilterLabri.setEnabled(true);
+				mPrefFilterLabri.setChecked(true);
+			}
+			else {
+				mPrefFilterLabri.setChecked(false);
+				mPrefFilterLabri.setEnabled(false);
+			}
+		}
 	}
 }
 
