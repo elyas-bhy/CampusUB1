7e1b1364b0e8328a79c2cda0e43644842fa1686a
TheLightBird <thelightbird@gmail.com>
2013-03-09 20:41:50 +0100

    Updating directory: added Jsoup HTML Parser, retrieve LaBRI directory by URL. Fixes #12

--- source/src/com/dev/campus/directory/DirectoryManager.java
+++ source/src/com/dev/campus/directory/DirectoryManager.java
@@ -1,14 +1,14 @@
 package com.dev.campus.directory;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.text.Normalizer;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import com.dev.campus.CampusUB1App;
 import com.dev.campus.directory.Contact.ContactType;
@@ -22,23 +22,21 @@ import com.unboundid.ldap.sdk.SearchResultEntry;
 import com.unboundid.ldap.sdk.SearchScope;
 import com.unboundid.ldap.sdk.controls.SimplePagedResultsControl;
 
-import android.text.Html;
-
 public class DirectoryManager {
 
 	private final String UB1_BASE_DN = "ou=people,dc=u-bordeaux1,dc=fr";
 	private final String UB1_LDAP_HOST = "carnet.u-bordeaux1.fr";
 	private final int UB1_LDAP_PORT = 389;
-	
+
 	private LDAPConnection LDAP;
 	private List<Contact> mLabriContacts;
-	
-	
+
+
 	public List<Contact> searchContact(String firstName, String lastName) throws LDAPException, IOException {
 		ArrayList<Contact> searchResult = new ArrayList<Contact>();
 		if (CampusUB1App.persistence.isSubscribedUB1()) 
-				searchResult.addAll(searchUB1(firstName, lastName));
-		
+			searchResult.addAll(searchUB1(firstName, lastName));
+
 		if (CampusUB1App.persistence.isSubscribedLabri()) {
 			if (mLabriContacts == null)
 				parseLabriDirectory();
@@ -87,7 +85,7 @@ public class DirectoryManager {
 
 		for (Contact c : mLabriContacts) {
 			if (removeAccents(c.getFirstName()).toLowerCase().contains(firstName)
-			 && removeAccents(c.getLastName()).toLowerCase().contains(lastName)) {
+					&& removeAccents(c.getLastName()).toLowerCase().contains(lastName)) {
 				matchingContacts.add(c);
 			}
 		}
@@ -95,82 +93,54 @@ public class DirectoryManager {
 	}
 
 	public void parseLabriDirectory() throws IOException {
-		String labriDirectory = "";
-		String filepath = "com/dev/campus/directory/DirectoryLabri.txt";
-
-		// Reading text file
-		try {
-			// InputStream ips = new FileInputStream(file);
-			InputStream ips = DirectoryManager.class.getClassLoader().getResourceAsStream(filepath);
-			InputStreamReader ipsr = new InputStreamReader(ips);
-			BufferedReader br = new BufferedReader(ipsr);
-			String line;
-			while ((line = br.readLine()) != null) {
-				labriDirectory += line + "\n";
-			}
-			br.close();
-		}
-		catch (Exception e) {
-			System.out.println(e.toString());
-		}
-
-
-		labriDirectory = labriDirectory.replaceAll("(\\r)|(\\n)|(\\t)", "");
-		labriDirectory = labriDirectory.replaceAll("&amp;", "&");
-
-		labriDirectory = labriDirectory.replaceFirst("<table border(.?)</table>", "");
-		labriDirectory = labriDirectory.replaceFirst("<tr(.*?)</tr>", "");
-
-		Pattern p = Pattern.compile("<td(.*?)>(.*?)</td>");
-		Matcher m = p.matcher(labriDirectory);
-
 		ArrayList<Contact> allContacts = new ArrayList<Contact>();
-		Contact contact = new Contact();
-		String buffer;
-		int i = 1;
-		
-		while (m.find()) {
-			buffer = m.group();
-			buffer = buffer.replaceAll("<td(.*?)>(.*?)</td>", "$2");
-			buffer = buffer.trim();
+		Document doc = Jsoup.connect("http://www.labri.fr/index.php?n=Annuaires.Noms&initiale=tout").get();
 
-			if (i % 8 == 1) { // Name
-				String name = buffer;
-				int offset = name.lastIndexOf(" "); // Split first name/last name with last space
-				// TODO control offset value
-				String lastName = name.substring(0, offset);
-				String firstName = name.substring(offset+1, name.length());
-				contact.setFirstName(htmlDecode(firstName));
-				contact.setLastName(htmlDecode(lastName));
-				contact.setType(ContactType.LABRI_CONTACT);
-			}
-			else if(i % 8 == 2) { // Email
-				buffer = buffer.replaceAll("<a href=\"mailto:([^\"]*)\"(.*)</a>", "$1");
-				contact.setEmail(htmlDecode(buffer));
-			}
-			else if (i % 8 == 3) { // Telephone, Default : "+33 (0)5 40 00 "
-				if (!buffer.equals("+33 (0)5 40 00")) {
-					buffer = buffer.trim();
-					contact.setTel(htmlDecode(buffer));
+		Elements tables = doc.select("table[border=1][cellpadding=4][cellspacing=0][width=100%]");
+		Element table = tables.first();
+
+		int i = 1;
+		String buffer;
+		Contact contact = new Contact();
+
+		Elements tds = table.getElementsByTag("td");
+		for (Element td : tds) {
+			buffer = td.text();
+
+			if (i > 8) {
+				if (i % 8 == 1) { // Name
+					String name = buffer;
+					int offset = name.lastIndexOf (" "); // Split first name/last name with last space
+					if (offset != -1) {
+						String lastName = name.substring(0, offset);
+						String firstName = name.substring(offset+1, name.length());
+						contact.setFirstName(firstName);
+						contact.setLastName(lastName);
+						contact.setType(ContactType.LABRI_CONTACT);
+					}
 				}
-			}
-			else if (i % 8 == 7) { // Website
-				buffer = buffer.replaceAll("<a href=\"http([^\"]*)\"(.*)</a>", "http$1");
-				contact.setWebsite(htmlDecode(buffer));
-			}
-			else if (i % 8 == 0 && i > 0) {
-				allContacts.add(contact);
-				contact = new Contact();
+				else if(i % 8 == 2) { // Email
+					String email = td.getElementsByTag("a").attr("href");
+					email = email.substring(7); // remove: "mailto:"
+					contact.setEmail(email);
+				}
+				else if (i % 8 == 3) { // Telephone, Default : "+33 (0)5 40 00 "
+					if (!buffer.equals("+33 (0)5 40 00")) {
+						contact.setTel(buffer);
+					}
+				}
+				else if (i % 8 == 7) { // Website
+					contact.setWebsite(td.getElementsByTag("a").attr("href"));
+				}
+				else if (i % 8 == 0 && i > 0) {
+					allContacts.add(contact);
+					contact = new Contact();
+				}
 			}
 			i++;
 		}
 
 		mLabriContacts = allContacts;
-	}
-
-
-	public String htmlDecode(String str) {
-		return Html.fromHtml(str).toString();
 	}
 
 	public String removeAccents(String str) {
