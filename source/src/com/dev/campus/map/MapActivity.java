0ec8f7a684b72a2c8d8fe26d60b4d465e72fc3f5
smijatov <stef.mijatovic@gmail.com>
2013-03-13 21:22:57 +0100

    minor refactoring, fixed typos

--- source/src/com/dev/campus/map/MapActivity.java
+++ source/src/com/dev/campus/map/MapActivity.java
@@ -119,7 +119,7 @@ public class MapActivity extends Activity implements LocationListener {
 	public void searchPosition(String input){
 		boolean found = false;
 		for (Position pos : Position.values()){
-			if(pos.getName().toLowerCase().equals(input.toLowerCase())){
+			if((pos.getName().toLowerCase().equals(input.toLowerCase()) || (pos.getName().toLowerCase().contains(input.toLowerCase())))){
 				ArrayList<Marker> markerType = null;
 				switch(pos.getType()) {
 				case BUILDING:
@@ -136,28 +136,7 @@ public class MapActivity extends Activity implements LocationListener {
 					if(pos.getmID().equals(marker.getId())){
 						marker.setVisible(true);
 						marker.showInfoWindow();
-					}
-				goToPosition(new LatLng(pos.getLat(),pos.getLng()),SEARCH_ZOOM);
-				found = true;
-				break;
-			}
-			if(pos.getName().toLowerCase().contains(input.toLowerCase())){
-				ArrayList<Marker> markerType = null;
-				switch(pos.getType()) {
-				case BUILDING:
-					markerType = mBuildingsMarkers;
-					break;
-				case RESTAURATION:
-					markerType = mRestaurationMarkers;
-					break;
-				case SERVICE:
-					markerType = mServicesMarkers;	
-					break;
-				}
-				for(Marker marker : markerType)
-					if(pos.getmID().equals(marker.getId())){
-						marker.setVisible(true);
-						marker.showInfoWindow();
+						break;
 					}
 				goToPosition(new LatLng(pos.getLat(),pos.getLng()),SEARCH_ZOOM);
 				found = true;
