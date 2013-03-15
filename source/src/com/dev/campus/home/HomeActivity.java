a47f791ab9f4b5179e1a0502731e3a97722f26d0
elyas-bhy <elyas.bhy@gmail.com>
2013-03-15 15:35:47 +0100

    Updated main menu layout, added establishment homepages.

--- source/src/com/dev/campus/home/HomeActivity.java
+++ source/src/com/dev/campus/home/HomeActivity.java
@@ -1,11 +1,16 @@
-package com.dev.campus;
+package com.dev.campus.home;
 
+import java.util.ArrayList;
+
+import com.dev.campus.CampusUB1App;
 import com.dev.campus.R;
+import com.dev.campus.SettingsActivity;
 import com.dev.campus.directory.DirectoryActivity;
 import com.dev.campus.event.EventsActivity;
 import com.dev.campus.map.MapActivity;
 import com.dev.campus.util.FilterDialog;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.ListActivity;
 import android.content.Intent;
@@ -13,11 +18,13 @@ import android.content.res.Resources;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
 public class HomeActivity extends ListActivity {
+	
+	private final String UB1_HOMEPAGE = "http://www.u-bordeaux1.fr/";
+	private final String LABRI_HOMEPAGE = "http://www.labri.fr/";
 
 	private FilterDialog mFilterDialog;
 	private Resources mResources;
@@ -30,14 +37,21 @@ public class HomeActivity extends ListActivity {
 
 		//ListView styling
 		ListView listView = getListView();
-		listView.setBackgroundResource(R.color.metal_grey);
-		listView.setDivider(mResources.getDrawable(R.color.dark_metal_grey));
+		listView.setBackgroundResource(R.color.grey_metal_dark);
+		listView.setDivider(mResources.getDrawable(R.color.grey_metal_dark_divider));
 		listView.setDividerHeight(1);
 		
-		//Setup adapter
-		String[] menu = mResources.getStringArray(R.array.main_menu);
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.simple_list_item, menu);
-		setListAdapter(adapter);
+		//Setup adapter items
+		ArrayList<HomeEntryItem> items = new ArrayList<HomeEntryItem>();
+		items.add(new HomeEntryItem(mResources.getString(R.string.events)));
+		items.add(new HomeEntryItem(mResources.getString(R.string.directory)));
+		items.add(new HomeEntryItem(mResources.getString(R.string.schedule)));
+		items.add(new HomeEntryItem(mResources.getString(R.string.map)));
+		items.add(new HomeEntryItem());	//separator
+		items.add(new HomeEntryItem(mResources.getString(R.string.ub1)));
+		items.add(new HomeEntryItem(mResources.getString(R.string.labri)));
+		
+		setListAdapter(new HomeEntryAdapter(this, items));
 	}
 
 	@Override
@@ -63,6 +77,14 @@ public class HomeActivity extends ListActivity {
 				startActivity(new Intent(HomeActivity.this, MapActivity.class));
 			else
 				Toast.makeText(this,mResources.getString(R.string.connection_failed), Toast.LENGTH_SHORT).show();  
+			break;
+		case 5: // UB1 website
+			Intent browserUB1 = new Intent(Intent.ACTION_VIEW, Uri.parse(UB1_HOMEPAGE));
+			startActivity(browserUB1);
+			break;
+		case 6: // LaBRI website
+			Intent browserLabri = new Intent(Intent.ACTION_VIEW, Uri.parse(LABRI_HOMEPAGE));
+			startActivity(browserLabri);
 			break;
 		default:
 			break;
---
+++ source/src/com/dev/campus/home/HomeEntryAdapter.java
@@ -0,0 +1,47 @@
+package com.dev.campus.home;
+
+import java.util.ArrayList;
+
+import com.dev.campus.R;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class HomeEntryAdapter extends ArrayAdapter<HomeEntryItem> {
+
+	private ArrayList<HomeEntryItem> mItems;
+	private LayoutInflater mLayoutInflater;
+
+	public HomeEntryAdapter(Activity context, ArrayList<HomeEntryItem> items) {
+		super(context, R.layout.simple_list_item, items);
+		mLayoutInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		mItems = items;
+	}
+
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View row = convertView;
+		final HomeEntryItem item = mItems.get(position);
+		
+		if (item != null) {
+			if (item.isSection()) {
+				row = mLayoutInflater.inflate(R.layout.home_list_item_separator, null);
+				row.setOnClickListener(null);
+				row.setOnLongClickListener(null);
+				row.setLongClickable(false);
+			} else {
+				row = mLayoutInflater.inflate(R.layout.home_list_item_entry, null);
+				final TextView title = (TextView) row.findViewById(R.id.home_list_item_entry_title);
+				title.setText(item.getTitle());
+			}
+		}
+		return row;
+	}
+
+}
