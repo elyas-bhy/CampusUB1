05c9278a57981831b5475da78567fd0ae526b923
Largaroth <rya.herb@gmail.com>
2013-03-25 16:57:30 +0100

    added favouriting with icon, also rearranged list view of events slightly

--- source/src/com/dev/campus/event/EventAdapter.java
+++ source/src/com/dev/campus/event/EventAdapter.java
@@ -3,12 +3,15 @@ package com.dev.campus.event;
 import java.util.List;
 
 import com.dev.campus.R;
+import com.dev.campus.event.Feed.FeedType;
+import com.google.android.gms.plus.model.people.Person.Image;
 
 import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class EventAdapter extends ArrayAdapter<Event> {
@@ -18,9 +21,10 @@ public class EventAdapter extends ArrayAdapter<Event> {
 	
 	private static class EventHolder {
 		TextView title;
+		TextView establishment;
 		TextView date;
-		TextView category;
 		TextView description;
+		ImageView star;
 	}
 	
 	public EventAdapter(Activity context, List<Event> events) {
@@ -40,9 +44,10 @@ public class EventAdapter extends ArrayAdapter<Event> {
 			
 			eventHolder = new EventHolder();
 			eventHolder.title = (TextView) row.findViewById(R.id.event_title);
+			eventHolder.establishment = (TextView) row.findViewById(R.id.event_estb);
 			eventHolder.date = (TextView) row.findViewById(R.id.event_date);
-			eventHolder.category = (TextView) row.findViewById(R.id.event_category);
 			eventHolder.description = (TextView) row.findViewById(R.id.event_description);
+			eventHolder.star = (ImageView) row.findViewById(R.id.event_star);
 			row.setTag(eventHolder);
 			
 		} else {
@@ -51,14 +56,23 @@ public class EventAdapter extends ArrayAdapter<Event> {
 		
 		Event event = mEvents.get(position);
 		
+		if(event.getSource().equals(FeedType.UB1_FEED))
+			eventHolder.establishment.setText("Bordeaux 1");
+		else
+			eventHolder.establishment.setText("LaBRI");
+		
 		if (event.isRead())
 			eventHolder.title.setTextColor(mContext.getResources().getColor(R.color.blue_title_read));
 		else
 			eventHolder.title.setTextColor(mContext.getResources().getColor(R.color.blue_title));
 		
+		if(event.isStarred())
+			eventHolder.star.setImageResource(R.drawable.ic_star_dark);
+		else
+			eventHolder.star.setImageResource(R.drawable.ic_unstar_dark);
+		
 		eventHolder.title.setText(event.getTitle());
 		eventHolder.date.setText(event.getStringDate());
-		eventHolder.category.setText(event.getCategory());
 		eventHolder.description.setText(event.getDescription());
 		
 		return row;
