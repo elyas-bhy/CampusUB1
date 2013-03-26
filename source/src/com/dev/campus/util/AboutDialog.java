48669b8c654c645bcb288cca38df3e38c9e789e6
elyas-bhy <elyas.bhy@gmail.com>
2013-03-26 22:37:07 +0100

    Added a contact action in about section, resolves #36

--- source/src/com/dev/campus/util/AboutDialog.java
+++ source/src/com/dev/campus/util/AboutDialog.java
@@ -5,19 +5,24 @@ import com.dev.campus.R;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.view.LayoutInflater;
 
 public class AboutDialog extends AlertDialog.Builder {
 
-	public AboutDialog(Context context) {
+	public AboutDialog(final Context context) {
 		super(context);
 		AlertDialog.Builder builder = new AlertDialog.Builder(context);
 
 		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		builder.setView(inflater.inflate(R.layout.dialog_about, null))
-		.setNegativeButton("Contact", new DialogInterface.OnClickListener() {
+		.setPositiveButton("Contact", new DialogInterface.OnClickListener() {
 			public void onClick(DialogInterface dialog, int id) {
-				//TODO
+				Intent emailIntent = new Intent(Intent.ACTION_SEND);
+				emailIntent.setType("plain/text");  
+				emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[] {"campusub1.dev@gmail.com"});
+				context.startActivity(Intent.createChooser(emailIntent, 
+											context.getResources().getString(R.string.menu_complete_action)));
 			}
 		});
 
